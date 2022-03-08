c07776bfb8782606a7a4b33f4a88085ba17645f3
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-08 10:47:16 +0200

    Configure application controller

--- db/migrate/20220308081644_devise_create_users.rb
+++ db/migrate/20220308081644_devise_create_users.rb
@@ -3,8 +3,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :photo
+  
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
