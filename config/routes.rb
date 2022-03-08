e49c1577818c058cd5d90dba7f81c99196f38033
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-08 12:14:04 +0200

    Update user routes and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
