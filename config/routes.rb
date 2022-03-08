51384937ee8622e80b7df803cfcad99239ab5634
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-08 15:15:38 +0200

    Configure recipes routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  resources :recipes
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
