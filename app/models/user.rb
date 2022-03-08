033c61badaa04ba6e7cdc5974744ccb22e834743
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-08 15:18:06 +0200

    Update user model with relationships and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  has_many :recipes, foreign_key: 'user_id'
+
+  validates :name, presence: true
 end
