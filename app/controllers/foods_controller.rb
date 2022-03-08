c337f67d0d382500cf2129c4115118564f683dd8
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-08 19:06:51 +0100

    Updated controller to delete and add new food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,30 +1,38 @@
 class FoodsController < ApplicationController
-  def index
-    @food = Food.all
-  end
-
-  def new
-    @food = Food.new
-  end
-
-  def create
-    @food = current_user.foods.new(food_params)
-    respond_to do |format|
-      format.html do
-        if @post.save
-          flash[:success] = 'Post was sucessfully created'
-          redirect_to "/foods"
-        else
-          flash.now[:error] = 'post was not created'
-          render :new
+        def index
+          @food = Food.all
         end
-      end
-    end
-  end
+      
+        def new
+          @food = Food.new
+        end
 
-  private
-
-  def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price)
-  end
+        def destroy
+            @food = Food.find(params[:id])
+            @food.destroy
+            flash[:success] = 'You have successfully deleted this post!'
+            redirect_to foods_path
+          end
+      
+        def create
+          @food = Food.new(food_params)
+          @food.user_id = current_user.id
+          respond_to do |format|
+            format.html do
+              if @food.save
+                flash[:success] = 'Post was sucessfully created'
+                redirect_to "/foods"
+              else
+                flash.now[:error] = 'post was not created'
+                render :new
+              end
+            end
+          end
+        end
+      
+        private
+      
+        def food_params
+          params.require(:food).permit(:name, :measurement_unit, :price)
+        end
 end
