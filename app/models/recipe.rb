9ee9a6e775a00be5ed2161f9c12e4d8bc696577c
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-10 13:38:43 +0200

    Update application, recipe, and user models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,13 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :ingredients, dependent: :destroy
+  has_many :foods, through: :ingredients
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :description, presence: true, length: { maximum: 1000 }
+
+  def add_ingredient(food, quantity)
+    already_exists = foods.find_by(name: food.name)
+    ingredients.create(food: food, quantity: quantity) unless already_exists
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  has_many :post
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :recipes, foreign_key: 'user_id'
+  has_many :foods, foreign_key: 'user_id'
 
   validates :name, presence: true
 
