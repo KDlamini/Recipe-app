6a04dcc56a21f610e8015d66dc40ad449d3992c0
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-10 13:32:28 +0200

    Create ingredients controller,model,helper, and views

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,62 @@
+class IngredientsController < ApplicationController
+  # GET /ingredients/new
+  def new
+    @foods = current_user.foods.all
+    @recipe = current_recipe
+    @ingredient = Ingredient.new
+  end
+
+
+  # GET /ingredients/1/edit
+  def edit
+  end
+
+  # POST /ingredients
+  def create
+    food = Food.find(params[:food_id])
+    @ingredient = current_recipe.add_ingredient(food, ingredient_params[:quantity])
+
+    respond_to do |format|
+      if @ingredient.save
+        format.html { redirect_to recipe_path(current_recipe), notice: "Food ingredient was successfully added." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /ingredients/1
+  def update
+    respond_to do |format|
+      if @ingredient.update(ingredient_params)
+        format.html { redirect_to ingredient_url(@ingredient), notice: "Ingredient was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /ingredients/1
+  def destroy
+    current_ingredient.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to recipe_path(current_recipe), notice: "Ingredient was successfully removed." }
+    end
+  end
+
+  private
+
+  def current_recipe
+    Recipe.find(params[:recipe_id])
+  end
+
+  def current_ingredient
+    Ingredient.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def ingredient_params
+    params.require(:ingredient).permit(:quantity)
+  end
+end
