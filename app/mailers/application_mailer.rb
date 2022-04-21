004375c7a1170df8ed86dd475ac975276874d897
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-21 16:46:22 +0200

    Update Devise email confirmation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'simo.nkosi552@gmail.com'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,17 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SEND_USERNAME'],
+    :password => ENV['SEND_PASSWORD'],
+    :domain => 'gmail.com',
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    open_timeout:         5,
+    read_timeout:         5
+}
