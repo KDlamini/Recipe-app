44ca095a6b139ef1432b09e6ea67d9b3b0c23727
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-22 11:40:18 +0200

    Update Devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root 'home#index'
 
   resources :users
